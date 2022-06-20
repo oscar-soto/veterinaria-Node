13529e55ce32ecf3beaed0946bfb8dbed864eb0b
Oscar Soto <oscarantonio.soto@gmail.com>
2022-06-20 17:17:48 -0400

    Fix: crear un middleware para autenticar usuario y validarlo

--- routes/veterinariosRoutes.js
+++ routes/veterinariosRoutes.js
@@ -1,12 +1,18 @@
 import express from "express";
 const router = express.Router();
-import { registrar, perfil, confirmar, autenticar } from "../controllers/veterinarioController.js";
+import {
+  registrar,
+  perfil,
+  confirmar,
+  autenticar,
+} from "../controllers/veterinarioController.js";
+
+import checkAuth from '../middleware/authMiddleware.js'
 
 router.post("/", registrar);
 router.get("/confirmar/:token", confirmar);
 router.post("/login", autenticar);
 
-
-router.get("/perfil", perfil);
+router.get("/perfil", checkAuth, perfil);
 
 export default router;
