9141e3149afab75d95967c3d528b6b183c895b1d
Oscar Soto <oscarantonio.soto@gmail.com>
2022-06-19 20:58:33 -0400

    Feat: crear primeras rutas y agregar controladores

---
+++ routes/veterinariosRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+const router = express.Router();
+import {registrar, perfil} from "../controllers/veterinarioController.js";
+
+router.get("/", registrar);
+
+router.get("/perfil", perfil);
+
+export default router;
