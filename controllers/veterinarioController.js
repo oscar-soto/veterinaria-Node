daeea5264d623c579e207e29acec1b31ee8f106c
Oscar Soto <oscarantonio.soto@gmail.com>
2022-06-20 13:09:56 -0400

    Fix: confirmar el usuario

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -7,8 +7,8 @@ const registrar = async (req, res) => {
   const existeEmail = await Veterinario.findOne({ email });
 
   if (existeEmail) {
-    const error = new Error('Usuario ya registrado')
-    return res.status(400).json({msg: error.message})
+    const error = new Error("Usuario ya registrado");
+    return res.status(400).json({ msg: error.message });
   }
 
   try {
@@ -26,4 +26,24 @@ const perfil = (req, res) => {
   res.json({ msg: "mostrando perfil" });
 };
 
-export { registrar, perfil };
+const confirmar = async (req, res) => {
+  const { token } = req.params;
+  const usuarioConfirmar = await Veterinario.findOne({ token });
+
+  if (!usuarioConfirmar) {
+    const error = new Error("Token no valido");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  try {
+    usuarioConfirmar.token = null;
+    usuarioConfirmar.confirmado = true;
+    await usuarioConfirmar.save();
+    
+    res.json({ msg: "confirmadon cuenta..." });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { registrar, perfil, confirmar };
