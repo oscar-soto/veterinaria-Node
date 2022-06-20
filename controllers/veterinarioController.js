23d2317d2f01f2264227e2b9be613cf8a65ae0b1
Oscar Soto <oscarantonio.soto@gmail.com>
2022-06-20 12:03:02 -0400

    Feat: prevenir usuarios duplicas y agregar un token

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -1,23 +1,29 @@
 import Veterinario from "../models/Veterinario.js";
 
 const registrar = async (req, res) => {
-  // const {email, password, nombre} = req.body;
+  const { email } = req.body;
+
+  // Prevenir Usarios registrados
+  const existeEmail = await Veterinario.findOne({ email });
+
+  if (existeEmail) {
+    const error = new Error('Usuario ya registrado')
+    return res.status(400).json({msg: error.message})
+  }
 
   try {
     // Guardar un Nuevo Veterinario
     const veterinario = new Veterinario(req.body);
     const veterinarioGuardado = await veterinario.save();
 
-
     res.json(veterinarioGuardado);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-
 };
 
-const perfil =  (req, res) => {
-  res.json({msg: "mostrando perfil"});
-}
+const perfil = (req, res) => {
+  res.json({ msg: "mostrando perfil" });
+};
 
 export { registrar, perfil };
